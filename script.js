92a1f1abf58b0dc31267257d539f8b08d50019f9
Bartlomiej Dlugosz <bartlomiejd15@gmail.com>
2022-04-11 12:45:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,105 @@
+let gameRunning = true
+let currentPlayer = 1
+let grid = document.getElementById("grid")
+let gridChildren = grid.children
+
+let endGame = playerWon => {
+    console.log("Player " + playerWon + " won!")
+    gameRunning = false
+}
+
+let createLine = (type, position) => { //type: 1 - horizontal, 2 - vertical, 3 - slanted positive, 4 - slanted negative
+    let newDiv = document.createElement("div")
+    newDiv.classList.add("winLine")
+    if (type === 2) {
+        newDiv.style.transform = "rotate(90deg)"
+    } else if (type === 3) {
+        newDiv.style.transform = "rotate(46deg)"
+        newDiv.style.width = "375%"
+    } else if (type === 4) {
+        newDiv.style.transform = "rotate(-46deg)"
+        newDiv.style.width = "375%"
+    }
+    gridChildren[position].appendChild(newDiv)
+}
+
+let checkHorizontal = player => {
+    for (let i = 0; i < 9; i += 3) {
+        let valid = true;
+        for (let j = 0; j < 3; j++) {
+            if (gridChildren[i + j].textContent !== ((player === 1 && "X") || "O")) {
+                valid = false
+                break
+            }
+        }
+        if (valid) {
+            createLine(1, i + 1)
+            return true
+        }
+    }
+    return false;
+}
+
+let checkVertical = player => {
+    for (let i = 0; i < 3; i++) {
+        let valid = true;
+        for (let j = 0; j < 3; j++) {
+            if (gridChildren[i + (j * 3)].textContent !== ((player === 1 && "X") || "O")) {
+                valid = false
+                break
+            }
+        }
+        if (valid) {
+            createLine(2, i + 3)
+            return true;
+        }
+    }
+    return false;
+}
+
+let checkSlanted = player => {
+    let valid = true
+    for (let i = 0; i < 9; i += 4) {
+        if (gridChildren[i].textContent !== ((player === 1 && "X") || "O")) {
+            valid = false
+            break
+        }
+    }
+    if (valid) {
+        createLine(3, 4)
+        return true
+    }
+
+    valid = true
+    for (let i = 2; i < 7; i += 2) {
+        if (gridChildren[i].textContent !== ((player === 1 && "X") || "O")) {
+            valid = false
+            break
+        }
+    }
+    if (valid) {
+        createLine(4, 4)
+        return true
+    }
+}
+
+let checkIfWon = player => {
+    if (checkHorizontal(player) || checkVertical(player) || checkSlanted(player)) {
+        endGame(player)
+    }
+}
+
+for (let i = 0; i < gridChildren.length; i++) {
+    let currentObject = gridChildren[i]
+    currentObject.addEventListener("click", e => {
+        if (gameRunning) {
+            if (currentObject.textContent === "") {
+                let newP = document.createElement("p")
+                newP.textContent = (currentPlayer === 1 && "X") || "O"
+                currentObject.appendChild(newP)
+                checkIfWon(currentPlayer)
+                currentPlayer = (currentPlayer === 1 && 2) || 1
+            }
+        }
+    })
+}
